4ab281a9ea2cbf6cfb4d865fe23ba5a148a4becc
jonyhy96 <hy352144278@gmail.com>
2023-04-27 16:03:32 +0800

    fix: gosu multi arch

--- bitnami/redis/5.0/debian-10/prebuildfs/build/install-gosu.sh
+++ bitnami/redis/5.0/debian-10/prebuildfs/build/install-gosu.sh
@@ -2,8 +2,9 @@
 
 VERSION="1.11"
 SHA256="0b843df6d86e270c5b0f5cbd3c326a04e18f4b7f9b8457fa497b0454c4b138d7"
+dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
 
-curl --silent -L "https://github.com/tianon/gosu/releases/download/${VERSION}/gosu-arm64" > "/usr/local/bin/gosu"
+curl --silent -L "https://github.com/tianon/gosu/releases/download/${VERSION}/gosu-$dpkgArch" > "/usr/local/bin/gosu"
 echo "$SHA256" "/usr/local/bin/gosu" | sha256sum --check
 chmod u+x "/usr/local/bin/gosu"
 mkdir -p "/opt/bitnami/licenses"
